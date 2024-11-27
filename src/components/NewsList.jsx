ec42f520c929eff4c3823c95cb04fd78c38c6494
israel litvak <lisrael1989@gmail.com>
2024-11-27 14:59:46 +0200

    Add news list cmp that go over the news with map and show every article that we have on the main page

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,3 +1,12 @@
-export function NewsList() {
-  return <div>NewsList</div>;
+/* eslint-disable react/prop-types */
+import { NewsPreview } from './NewsPreview';
+
+export function NewsList({ news }) {
+  return (
+    <div className="news-list">
+      {news.map((article) => (
+        <NewsPreview key={article.id} article={article} />
+      ))}
+    </div>
+  );
 }
