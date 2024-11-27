ec7fbd9b819ab7b5097273e8d0ba186c9c2d1138
israel litvak <lisrael1989@gmail.com>
2024-11-27 14:58:30 +0200

    Add logic and build preview cmp for every news

--- src/components/NewsPreview.jsx
+++ src/components/NewsPreview.jsx
@@ -1,3 +1,28 @@
-export function NewsPreview() {
-  return <div>NewsPreview</div>;
+/* eslint-disable react/prop-types */
+import { useNavigate } from 'react-router-dom';
+
+export function NewsPreview({ article }) {
+  const Navigate = useNavigate();
+
+  function ReadMore() {
+    Navigate(`/${article.id}`);
+  }
+  return (
+    <>
+      <div className="news-preview">
+        <div className="news-preview-content">
+          <h1 className="news-title">{article.title}</h1>
+          <p className="author">{article.author}</p>
+          <p className="news-text">{article.description}</p>
+          <p className="source">{article.source}</p>
+        </div>
+        <div className="news-preview-img">
+          <img className="news-img" src={article.img} alt={article.title} />
+          <button onClick={ReadMore} className="read-more-btn">
+            Read More
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
