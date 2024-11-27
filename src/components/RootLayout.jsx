35c343745cbcd6985cdaf5b230496671b86aa201
israel litvak <lisrael1989@gmail.com>
2024-11-27 15:00:12 +0200

    Add root layout + app basic header and footer

---
+++ src/components/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router';
+import { AppHeader } from './App-header/AppHeader.jsx';
+import { Footer } from './Footer/Footer.jsx';
+
+export function RootLayout() {
+  return (
+    <div className="main-layout">
+      <AppHeader />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
