056c9101781af4ddaaebc9847a02bcca7790ed28
israel litvak <lisrael1989@gmail.com>
2024-11-28 11:57:18 +0200

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,6 @@ export function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-logo">
-          <img className="footer-logo-img" src="/logo.png" alt="logo" />
-        </div>
         <div className="footer-copyright">
           <p>&copy; 2023 Your Company. All rights reserved.</p>
         </div>
