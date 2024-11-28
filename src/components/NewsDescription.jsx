2f08342f4fd3b3721c376edee9b8139e886e6660
israel litvak <lisrael1989@gmail.com>
2024-11-28 13:21:08 +0200

    Add news date

--- src/components/NewsDescription.jsx
+++ src/components/NewsDescription.jsx
@@ -4,6 +4,7 @@ export function NewsDescription() {
   const location = useLocation();
   const navigate = useNavigate();
   const article = location.state;
+  const formattedDate = new Date(article.published_at).toDateString();
 
   if (!article) {
     return <p>News not found</p>;
@@ -25,7 +26,15 @@ export function NewsDescription() {
           {article.author}
         </p>
         <p className="article-description">{article.description}</p>
-        <img className="article-img" src={article.image} alt="" />
+        <img
+          className="article-img"
+          src={
+            article.image ||
+            'https://plus.unsplash.com/premium_photo-1688561384438-bfa9273e2c00?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          }
+          alt=""
+        />
+        <p className="Data"> {formattedDate}</p>
         <p className="article-source">
           <span>Source:</span> {article.source}
         </p>
--- src/components/NewsPreview.jsx
+++ src/components/NewsPreview.jsx
@@ -8,17 +8,28 @@ export function NewsPreview({ article }) {
     Navigate(`/${article.id}`, { state: article });
   }
 
+  const newDate = new Date(article.published_at);
+  const formattedDate = newDate.toDateString();
+
   return (
     <>
       <div className="news-preview">
         <div className="news-preview-content">
           <h1 className="news-title">{article.title}</h1>
+          <p className="news-date">{formattedDate}</p>
           <p className="author">{article.author}</p>
           <p className="news-text">{article.description}</p>
           <p className="source">{article.source}</p>
         </div>
         <div className="news-preview-img">
-          <img className="news-img" src={article.image} alt={article.title} />
+          <img
+            className="news-img"
+            src={
+              article.image ||
+              'https://plus.unsplash.com/premium_photo-1688561384438-bfa9273e2c00?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt={article.title}
+          />
           <button onClick={ReadMore} className="read-more-btn">
             Read More
           </button>
