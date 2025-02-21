6baae748c02e78c3f22c3fe35bb8a30b4db8620e
Trygve Refvem <trygve.refvem@gmail.com>
2025-02-21 20:53:05 +0100

    Forenkler deploy.sh og setter miljøvariabler direkte

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# Sett arbeidskatalog
-cd /home/site/wwwroot
-
 # Sett miljøvariabler
-export NODE_ENV=production
 export PORT=8080
 export WEBSITES_PORT=8080
+export NODE_ENV=production
 export VITE_API_KEY=9957e8d299a340f6bf39141a7ae970a5
 export VITE_WORLD_NEWS_API_KEY=9957e8d299a340f6bf39141a7ae970a5
 export VITE_GOOGLE_GENERATIVE_AI_KEY=AIzaSyAPET1GbmQ0b9i_23Hv7vNwLO5OvDWyP8M
-
-# Kopier server.js til rot-mappen
-cp server/server.js .
 
 # Installer avhengigheter
 echo "Installing dependencies..."
@@ -24,9 +18,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Opprett logs-mappe
-mkdir -p logs
-
 # Start serveren
 echo "Starting server..."
-node server.js > logs/server.log 2>&1 
+node server.js 
