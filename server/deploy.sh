d3fffb1edeb1a8ade4fc7a0118e653b3946adc65
Trygve Refvem <trygve.refvem@gmail.com>
2025-02-21 21:13:07 +0100

    Forenkler deploy.sh ved å fjerne miljøvariabler

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Sett miljøvariabler
-export NODE_ENV=production
-export PORT=8080
-export WEBSITES_PORT=8080
-export VITE_API_KEY=9957e8d299a340f6bf39141a7ae970a5
-export VITE_WORLD_NEWS_API_KEY=9957e8d299a340f6bf39141a7ae970a5
-export VITE_GOOGLE_GENERATIVE_AI_KEY=AIzaSyAPET1GbmQ0b9i_23Hv7vNwLO5OvDWyP8M
-
 # Installer avhengigheter
 npm install
 
